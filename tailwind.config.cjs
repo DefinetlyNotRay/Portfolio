1fa75934b4c1d98a404e2816970a976550f97b92
DefinetlyNotRay <edgarmasstudio@gmail.com>
2023-10-04 18:30:11 +0700

    mobile navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,30 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#47566B",
+
+          secondary: "#E8ECF2",
+
+          accent: "#333942",
+
+          neutral: "#2a323c",
+
+          "base-100": "#1A1C1E",
+
+          info: "#3abff8",
+
+          success: "#36d399",
+
+          warning: "#fbbd23",
+
+          error: "#f87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
