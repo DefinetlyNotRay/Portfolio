d92d2251e9ff1de76fee03074be96f33dd3afd4a
DefinetlyNotRay <140494552+DefinetlyNotRay@users.noreply.github.com>
2023-10-05 19:03:07 +0700

    completed nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      backdropBlur: {
+        "4xl": "40px",
+      },
+    },
   },
   daisyui: {
     themes: [
